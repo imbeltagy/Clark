73c1f9a223f0d1d7c622c3a038ccb98d3ed0570e
Mohammed-Beltagy <justbeltagy@gmail.com>
2023-10-26 18:17:06 +0300

    Initialized Workspace and Configs

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./app.sass";
+import Hero from "./components/hero";
+import "/node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+}
+
+export default App;
