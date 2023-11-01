4ef0a49b6a19f1724376c7d110cd100ea99a0148
Mohammed-Beltagy <justbeltagy@gmail.com>
2023-11-01 15:26:05 +0200

    Added stroke and fill classes and svg in Blog Compnent

--- src/layout/blog/Index.jsx
+++ src/layout/blog/Index.jsx
@@ -35,14 +35,17 @@ const Blog = () => {
                 }}
               ></a>
               {/* Text */}
-              <p className="text-primary mt-2" style={{ letterSpacing: ".1em", fontSize: ".8rem" }}>
-                <span className="d-inline-block">JUNE 21, 2019</span>
-                <span className="d-inline-block mx-2" href="#">
+              <p className="mt-2" style={{ letterSpacing: ".1em", fontSize: ".8rem" }}>
+                <span className="text-primary d-inline-block">JUNE 21, 2019</span>
+                <a className="text-primary text-decoration-none d-inline-block mx-3" href="#">
                   ADMIN
-                </span>
-                <span className="d-inline-block" href="#">
-                  C 3
-                </span>
+                </a>
+                <a className="text-primary text-decoration-none d-inline-block" href="#">
+                  <svg height="1em" viewBox="0 0 512 512" className="fill-primary d-inline-block me-1">
+                    <path d="M64 0C28.7 0 0 28.7 0 64V352c0 35.3 28.7 64 64 64h96v80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H64z" />
+                  </svg>
+                  3
+                </a>
               </p>
               {/* Title */}
               <a
